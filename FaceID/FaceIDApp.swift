c238a1dea2591b95391dc74acd4b763b250c0737
Phoom Punpeng <phoomp14@gmail.com>
2023-03-29 10:24:05 +0700

    Minor fixes + firebase integration

--- FaceID/FaceIDApp.swift
+++ FaceID/FaceIDApp.swift
@@ -6,9 +6,18 @@
 //
 
 import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        FirebaseApp.configure()
+    }
+}
 
 @main
 struct FaceIDApp: App {
+    @NSApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
